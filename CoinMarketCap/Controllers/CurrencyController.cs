b401694427cc4a62507bbb325d0bb4e9f4d2d2ab
Denioo <deisygreazzy@gmail.com>
2019-06-19 18:39:56 +0500

    Создана модель, контроллер и представление.

---
+++ CoinMarketCap/Controllers/CurrencyController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CoinMarketCap.Models;
+using Newtonsoft.Json;
+
+namespace CoinMarketCap.Controllers
+{
+    public class CurrencyController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            HttpWebRequest Ticker = (HttpWebRequest)WebRequest.Create(string.Format("https://api.coinmarketcap.com/v1/ticker/"));
+
+            Ticker.Method = "Get";
+
+            HttpWebResponse webResponse = (HttpWebResponse)Ticker.GetResponse();
+
+            string currencyJson;
+            using (Stream stream = webResponse.GetResponseStream())   //modified from your code since the using statement disposes the stream automatically when done
+            {
+                StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8);
+                currencyJson = reader.ReadToEnd();
+            }
+
+            List<CurrencyModel> currencyModels = JsonConvert.DeserializeObject<List<CurrencyModel>>(currencyJson);
+            ViewBag.curr = currencyModels;
+
+            return View();
+        }
+    }
+}
