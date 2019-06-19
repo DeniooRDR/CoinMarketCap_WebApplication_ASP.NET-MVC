03b17b5adbe6fccd9c4545a16234191c724a5de4
Denioo <deisygreazzy@gmail.com>
2019-06-19 18:11:29 +0500

    Веб приложение работающее с CoinMarketCap. Выводит данные о криптовалюте, регистрация + вход. Проверка валидности

---
+++ CoinMarketCap/Controllers/CurrencyController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using Newtonsoft.Json;
+using CoinMarketCap.Models;
+
+namespace CoinMarketCap.Controllers
+{
+    public class CurrencyController : Controller
+    {
+        CurrencyContext db = new CurrencyContext();
+
+        public ActionResult CurrencyShow()
+        {
+            HttpWebRequest Ticker = (HttpWebRequest)WebRequest.Create(string.Format("https://api.coinmarketcap.com/v1/ticker/"));
+
+            Ticker.Method = "GET";
+
+            HttpWebResponse WebResponse = (HttpWebResponse)Ticker.GetResponse();
+
+            string CurrencyStringJson;
+            using (Stream stream = WebResponse.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8);
+                CurrencyStringJson = reader.ReadToEnd();
+            }
+
+            List<CurrencyModel> currencyModels = JsonConvert.DeserializeObject<List<CurrencyModel>>(CurrencyStringJson);
+
+            ViewBag.curr = currencyModels;
+            
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CoinMarketCap/Models/CurrencyContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CoinMarketCap.Models
+{
+    public class CurrencyContext : DbContext
+    {
+        public DbSet<CurrencyModel> CurrencyModels { get; set; }
+    }
+}
